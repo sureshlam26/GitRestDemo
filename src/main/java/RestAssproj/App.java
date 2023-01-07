8556b5b86239e485796dfdc21cfe8030f28996db
suresh <suresh.lam26@gmail.com>
2023-01-07 12:11:15 +0530

    first commit

---
+++ src/main/java/RestAssproj/App.java
@@ -0,0 +1,13 @@
+package RestAssproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
